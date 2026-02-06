b43259170bfb7253185dd9533c68eba405aff04f
Madaru69 <davidmutis4@gmail.com>
2026-02-06 20:00:50 +0100

    feat: Add dynamic scaling policy and document manual scaling validation 📈🛡️

--- asg.tf
+++ asg.tf
@@ -150,3 +150,17 @@ resource "aws_autoscaling_group" "moodle_asg" {
     aws_db_instance.moodle_db
   ]
 }
+
+# 3. Política de Escalado Dinámico (El "Cerebro")
+resource "aws_autoscaling_policy" "moodle_cpu_policy" {
+  name                   = "${var.project_name}-cpu-scaling-policy"
+  autoscaling_group_name = aws_autoscaling_group.moodle_asg.name
+  policy_type            = "TargetTrackingScaling"
+
+  target_tracking_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 50.0 # Objetivo: Mantener el CPU al 50%. Si sube, escala.
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "efs_id" {
   description = "ID del Sistema de Archivos EFS para referencia"
   value       = aws_efs_file_system.moodle_efs.id
 }
+
+output "alb_tg_arn" {
+  description = "ARN del Target Group para verificar salud de las instancias"
+  value       = aws_lb_target_group.alb_tg.arn
+}
